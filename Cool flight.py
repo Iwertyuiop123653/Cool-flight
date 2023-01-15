0afa637f068e371862f0883f96a17b15caa9a80d
IEATCODE <95064299+Iwertyuiop123653@users.noreply.github.com>
2023-01-15 11:06:07 -0800

    Update Cool flight.py

--- Cool flight.py
+++ Cool flight.py
@@ -1,15 +1,17 @@
 import time
-import config
-import webbrowser
-import requests
 import json
+import requests
+import webbrowser
 import colorama
+from tkinter import messagebox
 from colorama import Fore
 from colorama import Back
+
+
 colorama.init(autoreset=True)
 Flight_ICAO = input(f"{Fore.RED}What ICAO?\n: ")
 try:
-    response = requests.get(f"https://airlabs.co/api/v9/flights?api_key={config.api_key}&flight_icao={Flight_ICAO}")
+    response = requests.get(f"https://airlabs.co/api/v9/flights?api_key=19d63b03-421b-408e-925a-d50a54bc6b8a&flight_icao={Flight_ICAO}")
     response = json.loads(response.text)
     lat = response["response"][0]["lat"]
     lng = response["response"][0]["lng"]
@@ -17,7 +19,15 @@ try:
     url = f"https://www.latlong.net/c/?lat={lat}&long={lng}"
     print(f"{Fore.GREEN}{url}")
     print(f"{Fore.CYAN}{Back.BLUE}{speed} Knots")
-    time.sleep(3)
+    uri = "https://trueway-geocoding.p.rapidapi.com/ReverseGeocode"
+    querystring = {"location":f"{lat},{lng}","language":"en"}
+    headers = {
+    	"X-RapidAPI-Key": "4433f63431msh8d98becb55a842bp11ddb2jsn71a03c7f28d2",
+    	"X-RapidAPI-Host": "trueway-geocoding.p.rapidapi.com"
+    }
+    response = requests.request("GET", uri, headers=headers, params=querystring)
+    print(f'{Fore.BLUE}Address: {response.json()["results"][0]["address"]}')
+    time.sleep(5)
     webbrowser.open(url)
 except:
-    print(f"{Fore.RED}Mission Failed We'll get em next time")
+    messagebox.showerror("Cool Flight", "Task failed successfully.")
